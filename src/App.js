a536c7aa5883ff26d2d0506c8930d77a0c3bcff3
Krish Kedia <95727140+Kristar1@users.noreply.github.com>
2022-02-23 18:47:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ export default function App() {
         <Header title="My Todos List"/>
         <Routes>
           <Route
-            path="/"
+            path="/todolist"
             element={
               <>
                 <AddTodo addTodo={addTodo} />
